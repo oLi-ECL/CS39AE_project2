7a504a603b6db7bfc34be7b5dfd134d6f336c1d8
Hrelan <pokearth@hotmail.com>
2025-11-23 21:33:05 -0700

    changed EDA

--- pages/EDA_Gallery.py
+++ pages/EDA_Gallery.py
@@ -12,9 +12,9 @@ st.set_page_config(page_title='CS-39AE Project2',
 st.markdown(
 
 """
-Netflix 
-"""
-"""
+### **Netflix - Movies and TV Shows** 
+
+
 Dataset Analysis [Netflix Movies and TV Shows Comprehensive Catalogs](https://www.kaggle.com/datasets/kainatjamil12/niteee)
 """
 
@@ -41,11 +41,46 @@ def season_bargraph():
     fig.update_layout(
         title="Distribution of TV Show Seasons",
         xaxis_title="Number of Seasons",
-        yaxis_title="Count",
-        template="plotly_dark"
+        yaxis_title="Number of TV Shows",
+        template="plotly_white"
     )
 
-    return fig   # ← Important
+    return fig  
+
+
+def dur_movies_bargraph():
+    movies = df[df["type"] == "Movie"].copy()
+    movies["durations"] = movies["duration"].str.extract(r"(\d+)").astype(int)
+
+    bins = [0, 60, 90, 120, 150, 200, 300]
+    labels = ["0–60", "61–90", "91–120", "121–150", "151–200", "200+"]
+    movies["duration_bin"] = pd.cut(movies["durations"], bins=bins, labels=labels, right=False)
+
+ 
+    duration_counts = movies["duration_bin"].value_counts().sort_index()
+
+    fig = go.Bar(
+            x=duration_counts.index,
+            y=duration_counts.values,
+            marker_color="#E50914"   # Netflix red
+        )
+    
+
+    fig.update_layout(
+        title="Distribution of Movie Durations (Minutes)",
+        xaxis_title="Duration Range (Minutes)",
+        yaxis_title="Number of Movies",
+        template="plotly_white"
+    )
+
+    return fig
+
+
+
+
+
+
+
 
 
 col = st.columns((2,2), gap='medium')
@@ -53,3 +88,9 @@ col = st.columns((2,2), gap='medium')
 with col[0]:
     fig = season_bargraph()
     st.plotly_chart(fig, use_container_width=True)
+
+with col[1]:
+    fig = dur_movies_bargraph()
+    st.plotly_chart(fig, use_container_width=True)
+
+
